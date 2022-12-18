6486ada9d60790f78478d2aa7387e40960155657
Rifky Halsandrian <89020384+halsandriann@users.noreply.github.com>
2022-12-18 10:50:48 +0700

    updated push

--- src/data/example.js
+++ src/data/example.js
@@ -1,10 +1,17 @@
 module.exports = [
     {
-        "id": 1,
-        "title": "The Shawshank Redemption"
+      id: 1,
+      title: 'The Shawshank Redemption',
+      body: 'ini merupakan catatan ke-1',
+      created_at: new Date(),
+      updated_at: new Date(),
     },
     {
-        "id": 2,
-        "title": "Inception"
-    }
-]
+      id: 2,
+      title: 'Inception',
+      body: 'ini merupakan catatan ke-2',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ];
+  
--- src/index.js
+++ src/index.js
@@ -10,5 +10,116 @@ app.use(express.json());
 
 app.use('/api', routes);
 
+const data = require('./data/example.js');
+
+app.get('/', (req, res) => {
+  res.sendFile('/data/example.js', { root: __dirname });
+});
+
+//1. API dapat menyimpan catatan pada /data/example.js
+// coba dengan selain 1 dan 2
+app.post('/notes', (req, res) => {
+  const { id, title, body, created_at, updated_at } = req.body;
+
+  data.push({ id, title, body, created_at, updated_at });
+
+  res.status(201).send({
+    status: 'success',
+    message: 'catatan berhasil ditambahkan',
+    data: {
+      id: parseInt(id),
+      title,
+      body,
+      created_at,
+      updated_at,
+    },
+  });
+});
+
+//2. API dapat menampilkan seluruh catatan
+app.get('/notes', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    data: {
+      notes: data,
+    },
+  });
+});
+
+//3. API dapat menampilkan detail catatan
+app.get('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data.find((item) => item.id == id);
+
+  if (item) {
+    res.status(200).send({
+      status: 'success',
+      data: {
+        id: parseInt(id),
+        title: item.title,
+        body: item.body,
+        created_at: item.created_at,
+        updated_at: item.updated_at,
+      },
+    });
+  } else {
+    res.status(404).send({
+      status: 'fail',
+      message: 'Catatan tidak ditemukan',
+    });
+  }
+});
+
+//4. API dapat mengubah catatan
+app.put('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data.find((item) => item.id == id);
+
+  if (item) {
+    const { title, body } = req.body;
+    const created_at = item.created_at;
+    const updated_at = new Date();
+
+    item.title = title;
+    item.body = body;
+    item.updated_at = updated_at;
+
+    res.status(200).send({
+      status: 'success',
+      message: 'catatan berhasil diperbarui',
+      data: {
+        id: parseInt(id),
+        title: item.title,
+        body: item.body,
+        created_at,
+        updated_at: item.updated_at,
+      },
+    });
+  } else {
+    res.status(404).send({
+      status: 'fail',
+      message: 'Catatan tidak ditemukan',
+    });
+  }
+});
+
+//5. API dapat menghapus catatan
+app.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data.find((item) => item.id == id);
+  if (item !== -1) {
+    data.splice(item, 1);
+    res.status(200).send({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+  } else {
+    res.status(404).send({
+      status: 'fail',
+      message: 'Catatan tidak ditemukan',
+    });
+  }
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
